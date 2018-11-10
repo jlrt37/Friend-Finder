e28d18e5ff837c83bba299fa5809bb2e763e676e
jlrt37 <joshua.l.ragon@gmail.com>
2018-11-10 13:02:29 -0600

    trying to edit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,5 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
 
-	app.use(function (req, res) {
-		res.sendFile(path.join(__dirname + '/../public/home.html'));
-	});
+
 };
