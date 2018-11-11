c92d45556bfc038060115db4f0236c90fe3c2ea9
jlrt37 <joshua.l.ragon@gmail.com>
2018-11-10 19:04:35 -0600

    added friends to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,103 @@ var friendList = [
         "3",
         "4"
       ]
-    }
+    },
+    {
+        name: 'Ross',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/240px-David_Schwimmer_as_Ross_Geller.jpg',
+        scores: [
+          "2",
+          "1",
+          "4",
+          "5",
+          "3",
+          "3",
+          "2",
+          "4",
+          "1",
+          "3"
+        ]
+      },
+      {
+        name: 'Monica',
+        profilePic: 'https://sites.google.com/site/friends2365/_/rsrc/1472782816715/home/monica-geller/monica.jpg',
+        scores: [
+          "3",
+          "2",
+          "5",
+          "4",
+          "3",
+          "3",
+          "2",
+          "5",
+          "1",
+          "4"
+        ]
+      },
+      {
+        name: 'Rachel',
+        profilePic: 'https://pixel.nymag.com/imgs/daily/vulture/2014/12/17/17-rachel-green-jewish.w700.h700.jpg',
+        scores: [
+          "5",
+          "4",
+          "3",
+          "3",
+          "5",
+          "5",
+          "2",
+          "2",
+          "3",
+          "4"
+        ]
+      },
+      {
+        name: 'Chandler',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Matthew_Perry_as_Chandler_Bing.jpg/220px-Matthew_Perry_as_Chandler_Bing.jpg',
+        scores: [
+          "3",
+          "2",
+          "4",
+          "4",
+          "2",
+          "4",
+          "2",
+          "3",
+          "2",
+          "3"
+        ]
+      },
+      {
+        name: 'Phoebe',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Friendsphoebe.jpg/220px-Friendsphoebe.jpg',
+        scores: [
+          "5",
+          "5",
+          "3",
+          "3",
+          "5",
+          "5",
+          "1",
+          "2",
+          "4",
+          "5"
+        ]
+      },
+      {
+        name: 'Joey',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg/220px-Matt_LeBlanc_as_Joey_Tribbiani.jpg',
+        scores: [
+          "5",
+          "5",
+          "1",
+          "2",
+          "4",
+          "5",
+          "2",
+          "1",
+          "1",
+          "5"
+        ]
+      },
   ]
   
   module.exports = friendList;
